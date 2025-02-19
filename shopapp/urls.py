558fc5264c42639f4ac0abc934c0d075b597f0c4
Don25422 <danglaskimathi5@gmail.com>
2025-02-19 13:21:40 +0300

    first commit

---
+++ shopapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from shopapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('gallery/', views.images, name='gallery'),
+    path('about/', views.about, name='about'),
+]
